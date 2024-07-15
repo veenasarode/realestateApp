99358c81f5719a29493cb63dd8b11cc1738292f0
veenasarode <vu2vmn@gmail.com>
2024-07-15 11:11:51 +0530

    changes according figma

---
+++ src/main/java/io/bootify/my_app/exception/AgreementNotFoundException.java
@@ -0,0 +1,8 @@
+package io.bootify.my_app.exception;
+
+public class AgreementNotFoundException extends RuntimeException{
+
+    public AgreementNotFoundException(String s) {
+        super(s);
+    }
+}
